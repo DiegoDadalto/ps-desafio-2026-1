979c8465497b43ce18727c3e9444c2a32dac7d71
Diego Dadalto <diegodadi8@gmail.com>
2026-04-09 14:42:00 -0300

    :lipstick: feat - stylized more and added responsiveness

--- frontend/src/app/(site)/_components/Banner.tsx
+++ frontend/src/app/(site)/_components/Banner.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image'
 import bannerImg from '@/assets/img/bannerfull.jpg'
 
 export default function Banner() {
-    return <div className={styles.banner}>
-        <Image
-            src={bannerImg}
-            width={1920}
-            height={350}
-            alt="test"
-            className={styles.image} />
-    </div>
+  return <div className={styles.banner}>
+    <Image
+      src={bannerImg}
+      width={1920}
+      height={350}
+      alt="test"
+      className={styles.image} />
+  </div>
 }
